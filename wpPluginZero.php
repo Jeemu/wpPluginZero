c5b39c1ad6effcbd179263aeda5c234d2556fb50
Jeemu <jamesjosephtemba@gmail.com>
2020-04-01 15:55:16 +0300

    Added a file with plugin header comment

---
+++ wpPluginZero.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name:       My WP Plugin Zero
+ * Plugin URI:        https://github.com/Jeemu/wpPluginZero
+ * Description:       A plugin to do random stuffs.
+ * Version:           1.0.0
+ * Author:            Jeemu
+ * Author URI:        https://github.com/Jeemu
+ */
