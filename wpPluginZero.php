0e5fbce0b8ae296cdbb1e070ade64a914854f497
Jeemu <jamesjosephtemba@gmail.com>
2020-06-26 14:54:50 +0300

    Add a left-side admin menu item for the plugin

--- wpPluginZero.php
+++ wpPluginZero.php
@@ -13,8 +13,18 @@
  {
      $display = "";
      $display .= "<p>I can be seen!!!</p>";
-     $display .= '<div style="background-color: red; padding: 5px 3px;">This is a div element</div>';
+     $display .= '<div style="background-color: red; padding: 5px 6px;">This is a div element</div>';
 
      return $display;
  }
  add_shortcode('jeemu-display','jeemu_display_shortcode');
+
+ //Add an admin menu item and an admin option page, capturing and inserting options to certain web sections
+function jeemu_admin_menu_item()
+{
+    add_menu_page('PluginZero Page','Plugin Zero','manage_options','plugin-zero','jeemu_option_page_display','dashicons-marker',200);
+}
+add_action('admin_menu','jeemu_admin_menu_item');
+
+function jeemu_option_page_display()
+{}
