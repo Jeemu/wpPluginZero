a49bc13349c2003b2e9f06dbbc37c2e4dbb702fc
Jeemu <jamesjosephtemba@gmail.com>
2020-06-26 13:55:30 +0300

    Add a shortcode capability to the plugin

--- wpPluginZero.php
+++ wpPluginZero.php
@@ -7,3 +7,14 @@
  * Author:            Jeemu
  * Author URI:        https://github.com/Jeemu
  */
+
+ //Add a shortcode to display something to the client
+ function jeemu_display_shortcode()
+ {
+     $display = "";
+     $display .= "<p>I can be seen!!!</p>";
+     $display .= '<div style="background-color: red; padding: 5px 3px;">This is a div element</div>';
+
+     return $display;
+ }
+ add_shortcode('jeemu-display','jeemu_display_shortcode');
